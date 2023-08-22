bba0d46cd0e8a590889fc6f6495cc12d72d93afe
Cory Wattenberger <CWattenber@my.gcu.edu>
2023-08-22 06:26:08 -0400

    .

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 // Attempt to establish a connection with the SQL Server using PDO
 try {
     // Create a new PDO instance for SQL Server with the provided credentials and settings
-    $conn = new PDO("sqlsrv:server = tcp:cst323a3server.database.windows.net,1433; Database = BlogsDB", "snywdnrydv", "J60123G3V3J4ZN2B$");
+    $conn = new PDO("sqlsrv:server = tcp:cst323a3server.database.windows.net,1433; Database = BlogsDB-Act3", "snywdnrydv", "J60123G3V3J4ZN2B$");
 
     // Set the PDO error mode attribute to throw exceptionsfor error handling
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
