f06af8b58c7c249dc032a555f0b599d07264f223
Cory Wattenberger <CWattenber@my.gcu.edu>
2023-08-22 17:35:53 -0400

    .

--- login_form.php
+++ login_form.php
@@ -6,27 +6,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-    <!-- Google Identity Service -->
-    <script src="https://accounts.google.com/gsi/client"></script>
-
-    <!-- Google Sign-in Handler -->
-    <script>
-        // This function will be called when the user successfully signs in using Google
-        function onSignIn(credential) {
-            // If the credential object exists
-            if (credential) {
-                // Logging to console for debugging purposes
-                console.log("Google Sign-In successful");
-
-                // Extracting the id_token from the credential
-                var id_token = credential.credential;
-
-                // Redirecting the user to the PHP script to process the Google login 
-                // with the id_token as a parameter
-                window.location.href = 'process_google_login.php?idtoken=' + id_token;
-            }
-        }
-    </script>
 </head>
 
 <body>
@@ -82,11 +61,6 @@
 
     <!-- Close the database connection -->
     <?php $conn = null; ?>
-
-    <!-- Google Sign-In button setup, with client ID and callback function provided -->
-    <div id="g_id_onload" data-client_id="978762936400-13rs762kpapkc7f884r04rtfat8oiqar.apps.googleusercontent.com"
-        data-login_uri="https://activity-3-blog.azurewebsites.net/process_google_login.php" data-callback="onSignIn">
-    </div>
 
 </body>
 
